eddaaefea0573b1bdce4374576a69b050255f70b
situ5901 <situdancer9@gmail.com>
2025-03-12 17:41:34 +0530

    backend-o

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  res.status(200).json({ message: "Ho gai API Start BackEnd Live ho gya bhai...." });
+  res.status(200).json({ message: "Ho gai API Start BackEnd Live ho gya bhai.... Dak le" });
 });
 
 module.exports = router;
