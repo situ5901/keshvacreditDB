83adaf580cfb5187f3f05cd131e3dd17555f8bcf
situ5901 <situdancer9@gmail.com>
2025-04-13 13:15:08 +0530

    🔥situ working on CRM API working 1

--- routes/Crm.js
+++ routes/Crm.js
@@ -11,30 +11,48 @@ mongoose
 
 const db = mongoose.connection;
 
-router.get('/get-ramfin-leads', async (req, res) => {
+router.get("/get-all-leads", async (req, res) => {
   try {
-    const successfulLeads = await db.collection('userdb').find({
-      "apiResponse.message": "Lead created successfully."
-    }, {
-      projection: { phone: 1, _id: 0 }  
-    }).toArray();
+    const ramfinLeads = await db
+      .collection("userdb")
+      .find(
+        { "apiResponse.message": "Lead created successfully." },
+        { projection: { phone: 1, _id: 0 } },
+      )
+      .toArray();
 
-    const phoneNumbers = successfulLeads.map(lead => lead.phone);
-    const count = phoneNumbers.length;
+    const zypeLeads = await db
+      .collection("userdb")
+      .find(
+        {
+          "apiResponse.status": "ACCEPT",
+        },
+        { projection: { phone: 1, _id: 0 } },
+      )
+      .toArray();
+
+    const ramfinPhones = ramfinLeads.map((lead) => lead.phone);
+    const zypePhones = zypeLeads.map((lead) => lead.phone);
 
     res.status(200).json({
       success: true,
-      Partner: 'RamFin',
-      data: phoneNumbers,
-      count: count
+      message: "RamFin Create Leads",
+      RamFin: {
+        data: ramfinPhones,
+        total: ramfinPhones.length,
+      },
+
+      Zype: {
+        data: zypePhones,
+        total: zypePhones.length,
+      },
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: 'Error fetching leads',
-      error: error.message
+      message: "Error fetching leads",
+      error: error.message,
     });
   }
 });
-
 module.exports = router;
