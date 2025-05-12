8a6629aee4ce36ac85ba5a223b14dc06ea5406ab
Er.Oliver <situdancer9@gmail.com>
2025-05-12 11:16:40 +0530

    situ get-al

--- routes/Crm.js
+++ routes/Crm.js
@@ -6,63 +6,67 @@ require("dotenv").config();
 mongoose.set("strictQuery", true);
 const db = mongoose.connection;
 
-router.get("/get-all", async (req, res) => {
-  try {
-    const ramfinLeads = await db
-      .collection("userdb")
-      .find(
-        {
-          "apiResponse.RamFin.status": "1",
-          "apiResponse.RamFin.message": "Success",
-        },
-        {
-          projection: { phone: 1, _id: 0 },
-        },
-      )
-      .toArray();
-
-    const zypeLeads = await db
-      .collection("userdb")
-      .find(
-        { "apiResponse.fullResponse.status": "ACCEPT" },
-        { projection: { phone: 1, _id: 0 } },
-      )
-      .toArray();
-
-    const fatakPayLeads = await db
-      .collection("userdb")
-      .find(
-        { "apiResponse.message": "You are eligible." },
-        { projection: { phone: 1, _id: 0 } },
-      )
-      .toArray();
-
-    const ramfinPhones = ramfinLeads.map((lead) => lead.phone);
-    const zypePhones = zypeLeads.map((lead) => lead.phone);
-    const fatakPayPhones = fatakPayLeads.map((lead) => lead.phone);
-
-    res.status(200).json({
-      success: true,
-      message: "RamFin Create Leads",
-      RamFin: {
-        data: ramfinPhones,
-        total: ramfinPhones.length,
-      },
-      Zype: {
-        data: zypePhones,
-        total: zypePhones.length,
-      },
-      FatakPayPL: {
-        data: fatakPayPhones,
-        total: fatakPayPhones.length,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Error fetching leads",
-      error: error.message,
-    });
-  }
+router.get("/demo", async (req, res) => {
+  res.send("Hello from CRM");
 });
+
+// router.get("/get-all", async (req, res) => {
+//   try {
+//     const ramfinLeads = await db
+//       .collection("userdb")
+//       .find(
+//         {
+//           "apiResponse.RamFin.status": "1",
+//           "apiResponse.RamFin.message": "Success",
+//         },
+//         {
+//           projection: { phone: 1, _id: 0 },
+//         },
+//       )
+//       .toArray();
+//
+//     const zypeLeads = await db
+//       .collection("userdb")
+//       .find(
+//         { "apiResponse.fullResponse.status": "ACCEPT" },
+//         { projection: { phone: 1, _id: 0 } },
+//       )
+//       .toArray();
+//
+//     const fatakPayLeads = await db
+//       .collection("userdb")
+//       .find(
+//         { "apiResponse.message": "You are eligible." },
+//         { projection: { phone: 1, _id: 0 } },
+//       )
+//       .toArray();
+//
+//     const ramfinPhones = ramfinLeads.map((lead) => lead.phone);
+//     const zypePhones = zypeLeads.map((lead) => lead.phone);
+//     const fatakPayPhones = fatakPayLeads.map((lead) => lead.phone);
+//
+//     res.status(200).json({
+//       success: true,
+//       message: "RamFin Create Leads",
+//       RamFin: {
+//         data: ramfinPhones,
+//         total: ramfinPhones.length,
+//       },
+//       Zype: {
+//         data: zypePhones,
+//         total: zypePhones.length,
+//       },
+//       FatakPayPL: {
+//         data: fatakPayPhones,
+//         total: fatakPayPhones.length,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "Error fetching leads",
+//       error: error.message,
+//     });
+//   }
+// });
 module.exports = router;
