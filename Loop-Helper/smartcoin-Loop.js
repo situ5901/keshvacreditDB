f82507cfa582f9cba1804b216ca90b37fac5b56b
Er.Oliver <situdancer9@gmail.com>
2025-05-13 14:40:43 +0530

    situt SMNEW

--- Loop-Helper/smartcoin-Loop.js
+++ Loop-Helper/smartcoin-Loop.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 const qs = require("qs");
 require("dotenv").config();
 
-const MONGODB_URINEW = process.env.MONGODB_URINEW;
+const MONGODB_URIVISH = process.env.MONGODB_URIVISH;
 
 mongoose
-  .connect(MONGODB_URINEW)
+  .connect(MONGODB_URIVISH)
   .then(() => console.log("✅ MongoDB Connected Successfully"))
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
