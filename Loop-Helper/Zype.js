3e41664fb1d38da41755453f4e5973e90b66fd0e
Er.Oliver <situdancer9@gmail.com>
2025-06-22 14:13:03 +0530

    Asfg0092nhyhyn

--- Loop-Helper/Zype.js
+++ Loop-Helper/Zype.js
@@ -14,7 +14,7 @@ const UserDB = mongoose.model(
   new mongoose.Schema({}, { collection: "smcoll", strict: false }),
 );
 
-const MAX_PROCESS = 50000;
+const MAX_PROCESS = 100000;
 const BATCH_SIZE = 100; // Set your batch size
 const PartnerID = "a8ce06a0-4fbd-489f-8d75-345548fb98a8";
 const ELIGIBILITY_API =
