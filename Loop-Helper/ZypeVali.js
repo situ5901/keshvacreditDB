db6934b16bfa73aa36d365bff5b6da1d4aff5c1d
Er.Oliver <situdancer9@gmail.com>
2025-06-24 15:23:38 +0530

    smartco2345

--- Loop-Helper/ZypeVali.js
+++ Loop-Helper/ZypeVali.js
@@ -10,8 +10,8 @@ mongoose
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
 const UserDB = mongoose.model(
-  "zypeimp",
-  new mongoose.Schema({}, { collection: "zypeimp", strict: false }),
+  "smcoll",
+  new mongoose.Schema({}, { collection: "smcoll", strict: false }),
 );
 
 const BATCH_SIZE = 100;
