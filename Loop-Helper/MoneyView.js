0a8f29172e4d4f54d5b440d9645152cbe1208b5e
Er.Oliver <situdancer9@gmail.com>
2025-06-26 10:41:37 +0530

    smart45679983456780

--- Loop-Helper/MoneyView.js
+++ Loop-Helper/MoneyView.js
@@ -12,8 +12,8 @@ mongoose
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
 const UserDB = mongoose.model(
-  "userdb",
-  new mongoose.Schema({}, { collection: "userdb", strict: false }),
+  "MVcollection",
+  new mongoose.Schema({}, { collection: "MVcollection", strict: false }),
 );
 
 const Healthcheck_API = "https://atlas.whizdm.com/atlas/v1/health";
