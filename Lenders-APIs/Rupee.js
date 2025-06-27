9362a5ffc846f0f83dbcd3830dd200c7cec86726
Er.Oliver <situdancer9@gmail.com>
2025-06-27 11:19:22 +0530

    smart567998334346

--- Lenders-APIs/Rupee.js
+++ Lenders-APIs/Rupee.js
@@ -44,41 +44,6 @@ router.post("/partner/rupee/lead-create", async (req, res) => {
         dedupeResponse: dedupeRes.data,
       });
     }
-
-    const customer_lead_id = generate7DigitId();
-
-    const pushPayload = {
-      full_name: name || "",
-      mobile: phone || "",
-      email: email || "",
-      pancard: pan || "",
-      pincode: pincode || "",
-      income_type: employment || "1",
-      monthly_salary: income || "",
-      purpose_of_loan: "3",
-      loan_amount: loanAmount || "",
-      Partner_id,
-      customer_lead_id,
-    };
-
-    const pushRes = await axios.post(PushAPI_URL, pushPayload, {
-      headers: {
-        Username: Partner_id,
-        Auth: "a154c75adc5c96003c740668545c8ed59ff99f5ee520e8feb4b8087a41b2eb2a",
-        "Content-Type": "application/json",
-      },
-    });
-
-    return res.status(200).json({
-      pushResponse: pushRes.data,
-    });
-    if (pushRes.data.status === 0) {
-      return res.status(401).json({
-        success: false,
-        message: "❌ Lead push failed",
-        error: pushRes.data.error || "Unauthorized access",
-      });
-    }
   } catch (err) {
     return res.status(500).json({
       success: false,
