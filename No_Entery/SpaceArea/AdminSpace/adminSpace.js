25473482159a9cf0849c9626d5a8a2a698988bd2
Er.Oliver <situdancer9@gmail.com>
2025-07-08 19:15:29 +0530

    🔐Situ Updatew 988sd875678oi098760987#*%

--- No_Entery/SpaceArea/AdminSpace/adminSpace.js
+++ No_Entery/SpaceArea/AdminSpace/adminSpace.js
@@ -6,14 +6,3 @@ exports.healthCheck = (req, res) => {
   return res.status(200).json({ message: "Hello Admin" });
 };
 
-exports.login = (req, res) => {
-  const { adminId, adminPass } = req.body;
-  if (!adminId || adminId !== Id || !adminPass || Pass !== "situk") {
-    return res.status(400).json({ message: "Please provide name" });
-  }
-  const token = jwt.sign({ adminId }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-
-  return res.status(200).json({ message: "Hello " + adminId, token: token });
-};
