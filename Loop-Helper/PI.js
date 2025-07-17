94c55180e1347975bdfe8d8c990486ef1f661345
Er.Oliver <situdancer9@gmail.com>
2025-07-17 12:28:19 +0530

    🔐Situ_oiv0001034540A)

--- Loop-Helper/PI.js
+++ Loop-Helper/PI.js
@@ -3,9 +3,8 @@ const axios = require("axios");
 require("dotenv").config();
 
 const MONGODB_URIVISH = process.env.MONGODB_URIVISH;
-const TOKEN_API_URL = "https://vnotificationgw.uat.pointz.in/v1/auth/token";
-const LEAD_API_URL =
-  "https://vnotificationgw.uat.pointz.in/v1/leads/loans/create";
+const TOKEN_API_URL = "https://vnotificationgw.epifi.in/v1/auth/token";
+const LEAD_API_URL = "https://vnotificationgw.epifi.in/leads/loans/create";
 
 const BATCH_SIZE = 10;
 const REF_NAME = "PI";
