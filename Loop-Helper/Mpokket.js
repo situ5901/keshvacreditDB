8f9a87ad9e11b49c26b5e5b8ee9e7340483c1c15
Er.Oliver <situdancer9@gmail.com>
2025-08-22 13:08:12 +0530

    add co98y9625417

--- Loop-Helper/Mpokket.js
+++ Loop-Helper/Mpokket.js
@@ -10,8 +10,8 @@ mongoose
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
 const UserDB = mongoose.model(
-  "smcoll",
-  new mongoose.Schema({}, { collection: "smcoll", strict: false }),
+  "testdb",
+  new mongoose.Schema({}, { collection: "testdb", strict: false }),
 );
 
 const BATCH_SIZE = 100;
--- Loop-Helper/RamFin-Loop.js
+++ Loop-Helper/RamFin-Loop.js
@@ -20,8 +20,8 @@ mongoose
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
 const UserDB = mongoose.model(
-  "smcoll",
-  new mongoose.Schema({}, { collection: "smcoll", strict: false }),
+  "testdb",
+  new mongoose.Schema({}, { collection: "testdb", strict: false }),
 );
 
 async function dedupe(user) {
