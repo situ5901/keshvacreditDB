5672946f48b7b8cb3badfe1ce62840b3bf63212a
Er.Oliver <situdancer9@gmail.com>
2025-09-03 10:19:04 +0530

    first commi=456789o

--- Loop-Helper/PI.js
+++ Loop-Helper/PI.js
@@ -11,7 +11,7 @@ const LEAD_API_URL = "https://vnotificationgw.epifi.in/v1/leads/loans/create";
 const BATCH_SIZE = 10;
 const REF_NAME = "PI";
 
-const MONGODB_URINEW = process.env.MONGODB_URINEW;
+const MONGODB_URINEW = process.env.MONGODB_URINEW1;
 
 mongoose
   .connect(MONGODB_URINEW)
