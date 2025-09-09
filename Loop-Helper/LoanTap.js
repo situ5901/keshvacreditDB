3fee0575119da89bb3b16994bcd422f0458ddce1
Er.Oliver <situdancer9@gmail.com>
2025-09-09 10:30:12 +0530

    first co456.345wqds

--- Loop-Helper/LoanTap.js
+++ Loop-Helper/LoanTap.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 require("dotenv").config();
 
-const MONGODB_URINEW = process.env.MONGODB_URINEW1;
+const MONGODB_URINEW = process.env.MONGODB_URINEW;
 
 mongoose
   .connect(MONGODB_URINEW)
