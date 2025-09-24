b784da12777a0ebe198f4dcd2926acaf01b5bf50
Er.Oliver <situdancer9@gmail.com>
2025-09-24 10:40:52 +0530

    latest Com4faosdifhasdnfmasf590asf

--- Lenders-APIs/Faircent.js
+++ Lenders-APIs/Faircent.js
@@ -7,20 +7,20 @@ const fs = require("fs");
 const path = require("path");
 const UserDB = require("../routes/BL/BLSchema");
 
-const BASE_URL = "https://api.faircent.com";
-const APP_ID = "1cfa78742af22b054a57fac6cf830699";
-const APP_NAME = "KESHVACREDIT";
+// const BASE_URL = "https://api.faircent.com";
+// const APP_ID = "1cfa78742af22b054a57fac6cf830699";
+// const APP_NAME = "KESHVACREDIT";
 
 // Faircent config
-// const BASE_URL = "https://fcnode5.faircent.com";
-// const APP_ID = "b27b11e13af255ef90f7c1939dcab2d2";
-// const APP_NAME = "KESHVACREDIT";
+const BASE_URL = "https://fcnode5.faircent.com";
+const APP_ID = "b27b11e13af255ef90f7c1939dcab2d2";
+const APP_NAME = "KESHVACREDIT";
 
 // ------------------ Multer Disk Storage with Temp Folder ------------------
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Temp folder per request
-    const tempFolder = path.join(__dirname, "../temp", Date.now().toString());
+    const tempFolder = path.join(process.cwd(), "temp", Date.now().toString()); // AWS compatible
     if (!fs.existsSync(tempFolder)) {
       fs.mkdirSync(tempFolder, { recursive: true });
     }
