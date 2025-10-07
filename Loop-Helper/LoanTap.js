f14ba9456515374a89c9df8a44da9eaa56585f60
Er.Oliver <situdancer9@gmail.com>
2025-10-07 13:11:40 +0530

    asdfaout591asdaasdfasdfasdfasdfasdf

--- Loop-Helper/LoanTap.js
+++ Loop-Helper/LoanTap.js
@@ -161,7 +161,7 @@ async function runAllLeads() {
     let totalLeadsProcessed = 0;
 
     // --- YAHAN BATCH SIZE 100 KAR DIYA GAYA HAI ---
-    const BATCH_SIZE = 10;
+    const BATCH_SIZE = 50;
     // ----------------------------------------------
 
     console.log("🚀 Starting lead processing with a batch size of 100...");
