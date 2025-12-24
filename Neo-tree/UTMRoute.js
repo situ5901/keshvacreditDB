2c38b84f0655e032968d93bf80047e6e86a7520d
Oliver <er.situkumar@gmail.com>
2025-12-24 13:08:48 +0530

    Initial commit: clean code without large file

--- Neo-tree/UTMRoute.js
+++ Neo-tree/UTMRoute.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const UTMController = require("../Neo-tree/Treacking/UtmTrack");
 
 router.post("/utm", UTMController.generateUTM);
-// router.get("/", UTMController.trackUser);
 
 module.exports = router;
