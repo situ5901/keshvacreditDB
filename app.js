9e02203c6613a8e50f2db9a8e66cff213c3b61c5
Oliver <er.situkumar@gmail.com>
2026-01-23 11:41:50 +0530

    update app.js

--- app.js
+++ app.js
@@ -29,13 +29,7 @@ app.use(
 );
 
 app.use(logger("dev"));
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  }),
-);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(corsMiddleware);
