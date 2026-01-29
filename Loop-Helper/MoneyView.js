d593bf731a5af1799ad1d9996e41e04de4d4a07f
Oliver <er.situkumar@gmail.com>
2026-01-29 14:40:29 +0530

    MoneyView start

--- Loop-Helper/MoneyView.js
+++ Loop-Helper/MoneyView.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const path = require("path");
 const xlsx = require("xlsx");
 
-const MONGODB_URINEW = process.env.MONGODB_URINEW;
+const MONGODB_URINEW = process.env.MONGODB_RSUnity;
 
 mongoose
   .connect(MONGODB_URINEW)
@@ -12,8 +12,8 @@ mongoose
   .catch((err) => console.error("🚫 MongoDB Connection Error:", err));
 
 const UserDB = mongoose.model(
-  "comp",
-  new mongoose.Schema({}, { collection: "comp", strict: false }),
+  "smcoll",
+  new mongoose.Schema({}, { collection: "smcoll", strict: false }),
 );
 const Healthcheck_API = "https://atlas.whizdm.com/atlas/v1/health";
 const TOKEN_API = "https://atlas.whizdm.com/atlas/v1/token";
